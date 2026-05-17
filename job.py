9bac4a6affcbba3c311aa3ca26a8beba1387c431
ROSHINI.D <134485213+roshcheeku@users.noreply.github.com>
2026-05-17 13:01:40 +0530

    Update job.py

--- job.py
+++ job.py
@@ -5,9 +5,9 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 # API Configuration  
-GOOGLE_API_KEY = "AIzaSyDd2Njm34U_vU_uYOPqFjI0S_yrUOMLhyo"  
-CUSTOM_SEARCH_ENGINE_ID = "3741929af179a4b17"
-JOOBLE_API_URL = "https://jooble.org/api/3f399c3d-2800-474c-bdba-4195187347eb"
+GOOGLE_API_KEY = ""  
+CUSTOM_SEARCH_ENGINE_ID = ""
+JOOBLE_API_URL = ""
 
 # Function to fetch job postings from Google Custom Search
 def fetch_google_jobs(queries=None):
